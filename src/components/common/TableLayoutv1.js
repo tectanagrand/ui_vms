8d78a935693796bbe0f9f5f75fcd3850fb5a5240
tectanagrand <rtektano@gmail.com>
2024-01-24 14:13:24 +0700

    ref : table layout variadic column

---
+++ src/components/common/TableLayoutv1.js
@@ -0,0 +1,149 @@
+import {
+  Table,
+  TableCell,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableFooter,
+  Box,
+  TablePagination,
+  TableContainer,
+  makeStyles,
+  Typography,
+} from '@mui/material';
+import TableRows from './TableRowsv1';
+import { useState, useEffect } from 'react';
+import SearchBar from '@mkyy/mui-search-bar';
+
+export default function TableLayout({ data, header, lengthRow, detailsComp }) {
+  //header format
+  /*
+  [
+    {
+      data : {key property of data},
+      header : {header name},
+      renderCell? : (row) => {
+        return <rendered cell>
+      } 
+    },
+    ...
+  ]
+
+  */
+  const [rows, setRow] = useState(data);
+  const [searched, setSearched] = useState('');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [empty, setEmpty] = useState(0);
+  const handleChangePage = (e, newpage) => {
+    setPage((page) => {
+      setEmpty(
+        newpage > 0
+          ? Math.max(0, rowsPerPage - rows.slice(newpage * rowsPerPage, newpage * rowsPerPage + rowsPerPage).length)
+          : 0
+      );
+      return newpage;
+    });
+  };
+
+  useEffect(() => {
+    setEmpty(Math.max(0, rowsPerPage - rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).length));
+  }, []);
+
+  useEffect(() => {
+    setRow(data);
+    setEmpty(Math.max(0, rowsPerPage - data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).length));
+  }, [data]);
+  const requestSearch = (searchedVal) => {
+    const filteredRows = data.filter((row) => {
+      let check = false;
+      Object.values(row).map((item) => {
+        // console.log(item);
+        if (item === null) {
+          return;
+        }
+        if (item.toString().toLowerCase().includes(searchedVal.toString().toLowerCase())) {
+          check = true;
+          return;
+        }
+      });
+      return check;
+    });
+    setRow(filteredRows);
+  };
+
+  const cancelSearch = () => {
+    setSearched('');
+    requestSearch(searched);
+  };
+
+  let idx = 0;
+  return (
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', margin: 1 }}>
+        <SearchBar
+          value={searched}
+          onChange={requestSearch}
+          onCancelResearch={cancelSearch}
+          onSearch={(search) => setSearched(search)}
+        />
+      </Box>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {data.length != 0 && 'details' in data[0] && <TableCell key="details">details</TableCell>}
+              {header.map((item) => (
+                <TableCell key={item.header}>{item.header}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.length != 0 &&
+              (rowsPerPage > 0 ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : rows).map(
+                (item) => (
+                  <TableRows key={item.id} header={header} row={item} length={lengthRow} DetComp={detailsComp} />
+                )
+              )}
+            {empty > 0 && data.length != 0 && (
+              <TableRow style={{ height: 74.5 * empty }}>
+                <TableCell colSpan={lengthRow} />
+              </TableRow>
+            )}
+            {data.length == 0 && (
+              <TableRow style={{ height: 74.5 * empty }}>
+                <TableCell colSpan={lengthRow}>
+                  <Typography variant="h5" sx={{ textAlign: 'center' }}>
+                    Empty Data
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Table>
+        <TableHead></TableHead>
+        <TableBody></TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={lengthRow}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </>
+  );
+}
---
+++ src/components/common/TableRowsv1.js
@@ -0,0 +1,52 @@
+import { KeyboardArrowDown, KeyboardArrowUp, Check, DoDisturb, Edit } from '@mui/icons-material';
+import { TableRow, TableCell, IconButton, Collapse, Typography, TextField, Tooltip } from '@mui/material';
+import { useState } from 'react';
+
+// row => object
+// header => array of object
+
+export default function Row({ header, row, length, DetComp }) {
+  const [open, setOpen] = useState(false);
+  const clickDetails = () => {
+    setOpen(!open);
+  };
+  let x = 0;
+  return (
+    <>
+      <TableRow key={row.id + 'row'}>
+        {DetComp !== undefined && (
+          <TableCell key={row.id + 'details'}>
+            <IconButton key={row.id + 'detailsButton'} onClick={clickDetails}>
+              {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+            </IconButton>
+          </TableCell>
+        )}
+        {header.map((item) => {
+          const renderFun = item.renderCell;
+          if (renderFun === undefined) {
+            return (
+              <TableCell key={row.id + item.data} align="left">
+                <Typography key={row.id + item.data + 'text'}>{row[item.data]}</Typography>
+              </TableCell>
+            );
+          } else {
+            return (
+              <TableCell key={row.id + item.header} align="center">
+                {renderFun(row)}
+              </TableCell>
+            );
+          }
+        })}
+      </TableRow>
+      {DetComp !== undefined && (
+        <TableRow key={row.id + 'rowDetail'}>
+          <TableCell key={row.id + 'detailsCol'} colSpan={length} sx={{ padding: 0, margin: 0 }}>
+            <Collapse key={row.id + 'collapsible'} in={open} timeout="auto" unmountOnExit>
+              <DetComp data={row} />
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      )}
+    </>
+  );
+}
